713c8bc0767b0030e6aa746a40f91337dc898bfe
franklin <franklin.oliveira096@gmail.com>
2025-08-30 20:58:02 -0300

    Criando css e html base

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -5,4 +5,14 @@ def index(request):
         request,
        'blog/pages/index.html'
     )
+def post(request):
+    return render(
+        request,
+       'blog/pages/post.html'
+    )
+def page(request):
+    return render(
+        request,
+       'blog/pages/page.html'
+    )
 # Create your views here.
