c002f91d9a91c8136bc2ebb305993269cc54e10a
franklin <franklin.oliveira096@gmail.com>
2025-09-06 12:15:34 -0300

    Criando o model.Post, modificando o post e page admin para inclusão do created e updated_by, redimensionamento das imagens do post

--- djangoapp/blog/admin.py
+++ djangoapp/blog/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from blog.models import Tag, Category, Page
+from blog.models import Tag, Category, Page, Post
+from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
@@ -24,7 +25,8 @@ class CategoryAdmin(admin.ModelAdmin):
     }
 
 @admin.register(Page)
-class PageAdmin(admin.ModelAdmin):
+class PageAdmin(SummernoteModelAdmin):
+    summernote_fields = ('content',)
     list_display = 'id', 'title', 'is_published',
     list_display_links = 'title',
     search_fields = 'id', 'title', 'slug',
@@ -35,4 +37,27 @@ class PageAdmin(admin.ModelAdmin):
     prepopulated_fields = {
         "slug": ('title',),
     }
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+    summernote_fields = ('content',)
+    list_display = 'id', 'title', 'is_published', 'created_by',
+    list_display_links = 'title',
+    search_fields = 'id', 'slug', 'title', 'excerpt', 'content', 
+    list_per_page = 50
+    list_filter = 'category', 'is_published',
+    list_editable = 'is_published',
+    ordering = '-id',
+    readonly_fields = 'created_at', 'updated_at', 'updated_by', 'created_by', 
+    prepopulated_fields = {
+        "slug": ('title',),
+    }
+    autocomplete_fields = 'tags', 'category',
+
+    def save_model(self, request, obj, form, change):
+        if change:
+            obj.updated_by = request.user
+        else:
+            obj.created_by = request.user
+        obj.save()
 # Register your models here.
--- djangoapp/blog/models.py
+++ djangoapp/blog/models.py
@@ -1,5 +1,14 @@
 from django.db import models
 from utils.rands import slugify_new
+from django.contrib.auth.models import User
+from utils.images import resize_image
+from django_summernote.models import AbstractAttachment
+
+# class PostAttachment(AbstractAttachment):
+#     def save(self, *args, **kwargs):
+#         if not self.name:
+#             self.name = self.file.name
+#         super().save(*args, **kwargs)
 
 class Tag(models.Model):
     class Meta:
@@ -9,7 +18,7 @@ class Tag(models.Model):
     name = models.CharField(max_length=255)
     slug = models.SlugField(
         unique=True,
-        default='None',
+        default='',
         null=True,
         blank=True,
         max_length=255
@@ -65,4 +74,60 @@ class Page(models.Model):
 
     def __str__(self) -> str:
         return self.title
-# Create your models here.
+
+
+class Post(models.Model):
+    class Meta:
+        verbose_name = 'Post'
+        verbose_name_plural = 'Posts'
+
+    title = models.CharField(max_length=65,)
+    slug = models.SlugField(
+        unique=True,
+        default='',
+        null=True,
+        blank=True,
+        max_length=255
+    )
+    excerpt = models.CharField(max_length=150)
+    is_published = models.BooleanField(default=False, help_text='Este campo precisará estar marcado, para o post ser exibido publicamente.')
+    content = models.TextField()
+    cover = models.ImageField(upload_to='posts/%Y/%m/', blank=True, default='')
+    cover_in_post_content = models.BooleanField(default=True, help_text='Se marcado, exibirá a capa dentro do post.',)
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        blank=True, null=True, related_name='post_created_by'
+    )
+    updated_at = models.DateTimeField(auto_now=True)
+    updated_by = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        blank=True, null=True, related_name='post_updated_by'
+    )
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True, blank=True, default=None,
+    )
+    tags = models.ManyToManyField(Tag, blank=True, default='')
+
+    def __str__(self):
+        return self.title
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify_new(self.title, 4)
+        
+        current_cover_name = str(self.cover.name)
+        super_save = super().save(*args, **kwargs)
+        cover_changed = False
+
+        if self.cover:
+            cover_changed = current_cover_name != self.cover
+        
+        if cover_changed:
+            resize_image(self.cover, 900)
+        
+        return super_save
+
+
