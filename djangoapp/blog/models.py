1e0e7ae86ea494bd094350fde9c664c553016d65
franklin <franklin.oliveira096@gmail.com>
2025-09-13 12:46:41 -0300

    Completando o model Page, criando os titulos da pagina e adicionando o http404 e migrando as views pra class based view

--- djangoapp/blog/models.py
+++ djangoapp/blog/models.py
@@ -93,7 +93,7 @@ class Page(models.Model):
 
 class PostManager(models.Manager):
     def get_published(self):
-        return self.filter(is_published=True).order_by('-pk')
+        return self.filter(is_published=True)
 
 class Post(models.Model):
     class Meta:
